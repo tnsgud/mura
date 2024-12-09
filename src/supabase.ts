26d4c44f3964345d9591506537a9d1f13deacc94
tnsgud <qkrtnsgud0229@gmail.com>
2024-12-10 02:27:31 +0900

    ♻: supabase 객체 분리

---
+++ src/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '../database.types.ts';
+
+export const supabase = createClient<Database>(
+  `${import.meta.env.VITE_SUPABASE_PROJECT_URL}`,
+  `${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+);
