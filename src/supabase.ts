e87681f8e0248f799e2c43d82c6901d6da0d4436
tnsgud <qkrtnsgud0229@gmail.com>
2024-12-12 13:49:07 +0900

    ♻: import path 변경

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '../database.types.ts';
+import { Database } from './database.types.ts';
 
 export const supabase = createClient<Database>(
   `${import.meta.env.VITE_SUPABASE_PROJECT_URL}`,
