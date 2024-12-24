ed783189ab0f7300928629b08ec2414438db8927
tnsgud <qkrtnsgud0229@gmail.com>
2024-12-25 03:39:32 +0900

    ♻: type을 간편하게 작성하기 위해서 실험

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,16 +1,20 @@
 import { supabase } from '@/supabase';
 import { useEffect, useState } from 'react';
 import Project from '@components/project';
-import { QueryData } from '@supabase/supabase-js';
+import { Tables } from '@/database.types';
+
+type PartialProject = Omit<
+  Tables<'projects'>,
+  'created_at' | 'updated_at' | 'deleted_at' | 'manager_id'
+>;
 
 const projectsQuery = supabase
   .from('projects')
-  .select(`id, name, description, signature_code`);
-
-type ProjectsResult = QueryData<typeof projectsQuery>;
+  .select(`id, name, description, signature_code`)
+  .returns<PartialProject[]>();
 
 function Projects() {
-  const [projects, setProjects] = useState<ProjectsResult>([]);
+  const [projects, setProjects] = useState<PartialProject[]>([]);
 
   useEffect(() => {
     projectsQuery.then((query) => {
@@ -32,7 +36,7 @@ function Projects() {
   return (
     <div className='grid grid-cols-3 gap-x-5 gap-y-5 px-5 py-5'>
       {projects.map((p) => (
-        <Project data={p} />
+        <Project key={`project-${p.signature_code}-${p.id}`} data={p} />
       ))}
     </div>
   );
