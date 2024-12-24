d659632c5418dbdf15adcf6aaa18bffd99aa9089
tnsgud <qkrtnsgud0229@gmail.com>
2024-12-24 10:35:51 +0900

    📦️: import alias 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, './src') },
+      { find: '@routes', replacement: resolve(__dirname, './src/routes') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, './src/components'),
+      },
+    ],
+  },
+  plugins: [react(), tsConfigPaths()],
+});
